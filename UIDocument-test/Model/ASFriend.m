e430907c06e88e7dfad702f592016ff7fd6f4b91
Brovko Roman <brovkoroman@Brovko-Romans-Mac-Mini.local>
2014-02-27 12:33:37 +0200

    add model

---
+++ UIDocument-test/Model/ASFriend.m
@@ -0,0 +1,16 @@
+//
+//  ASFriend.m
+//  UIDocument-test
+//
+//  Created by Brovko Roman on 27.02.14.
+//  Copyright (c) 2014 AshberrySoft. All rights reserved.
+//
+
+#import "ASFriend.h"
+
+@implementation ASFriend
+
+@synthesize name = _name;
+@synthesize birthdate = _birthdate;
+
+@end
